cda78cfd4cdf025ce581e6a5144360bbe7204b63
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2017-10-02 17:07:17 -0700

    Apply derivative filter on cam position data, when data is loaded
    Apply derivative filter on cam position data, when data is loaded. This
    way, we can tune derivative filter parameters (window size, polynomial
    order) after experiment data is collected :)

--- Experiment postprocessing/Helper functions/IO/loadRealExperimentData.m
+++ Experiment postprocessing/Helper functions/IO/loadRealExperimentData.m
@@ -2,23 +2,30 @@
 % Loads (real, ie. from CF drones) experiment data from .npz files into a struct which contains the accel measured from the camera, accel and gyro measured from the drone's IMU...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function data = loadRealExperimentData(logInfo, logFolder)
+function data = loadRealExperimentData(logInfo, logFolder, derivPolyOrder, derivWinSize, movingAvgFiltWinSize)
+	if nargin<2 || isempty(logFolder), logFolder = [fileparts(mfilename('fullpath')) '/../../../Data collection/log/' logInfo.ch]; end
+	if nargin<3 || isempty(derivPolyOrder), derivPolyOrder = 2; end
+	if nargin<4 || isempty(derivWinSize), derivWinSize = 21; end
+	if nargin<5 || isempty(movingAvgFiltWinSize), movingAvgFiltWinSize = 30; end
     magnitudes = {'X'; 'Y'; 'Z'};
-    if nargin < 2 || isempty(logFolder)
-        logFolder = [fileparts(mfilename('fullpath')) '/../../../Data collection/log/' logInfo.ch];
-    end
     
     data = repmat(cell2struct([repmat({cell2struct(cell(3,1), magnitudes)}, 6,1); cell(2,1)], {'a_cam','a_UAV','gyro_UAV','a_cam_orig','a_UAV_orig','gyro_UAV_orig', 'tInterv', 'tCropInds'}), length({logInfo.datetime}), 1);
     for j = 1:length({logInfo.datetime})
         for i = 1:size(magnitudes,1)
+			% Read logs from files
 			pos_cam_orig = readNPZ([logFolder '/' logInfo(j).datetime '/log_p' magnitudes{i} '_cam_' logInfo(j).ch '_' strrep(logInfo(j).datetime, ' ','_') '.npz']);
 			vel_cam_orig = readNPZ([logFolder '/' logInfo(j).datetime '/log_v' magnitudes{i} '_cam_' logInfo(j).ch '_' strrep(logInfo(j).datetime, ' ','_') '.npz']);
             accel_cam_orig = readNPZ([logFolder '/' logInfo(j).datetime '/log_a' magnitudes{i} '_cam_' logInfo(j).ch '_' strrep(logInfo(j).datetime, ' ','_') '.npz']);
             accel_UAV_orig = readNPZ([logFolder '/' logInfo(j).datetime '/log_a' magnitudes{i} '_world_' logInfo(j).ch '_' strrep(logInfo(j).datetime, ' ','_') '.npz']); accel_UAV_orig.measured = accel_UAV_orig.measured - accel_UAV_orig.measured(1);
             gyro_UAV_orig = readNPZ([logFolder '/' logInfo(j).datetime '/log_gyro' magnitudes{i} '_' logInfo(j).ch '_' strrep(logInfo(j).datetime, ' ','_') '.npz']);
-            accel_UAV_dsamp = struct('tFloat',accel_cam_orig.tFloat, 'measured',interp1(accel_UAV_orig.tFloat, movingAvgFilter(30, accel_UAV_orig.measured), accel_cam_orig.tFloat)); % accel_UAV_dsamp.measured(isnan(accel_UAV_dsamp.measured))=0;
-            gyro_UAV_dsamp = struct('tFloat',accel_cam_orig.tFloat, 'measured',interp1(gyro_UAV_orig.tFloat, movingAvgFilter(30, gyro_UAV_orig.measured), accel_cam_orig.tFloat)); % gyro_UAV_dsamp.measured(isnan(gyro_UAV_dsamp.measured))=0;
+            accel_UAV_dsamp = struct('tFloat',accel_cam_orig.tFloat, 'measured',interp1(accel_UAV_orig.tFloat, movingAvgFilter(movingAvgFiltWinSize, accel_UAV_orig.measured), accel_cam_orig.tFloat)); % accel_UAV_dsamp.measured(isnan(accel_UAV_dsamp.measured))=0;
+            gyro_UAV_dsamp = struct('tFloat',accel_cam_orig.tFloat, 'measured',interp1(gyro_UAV_orig.tFloat, movingAvgFilter(movingAvgFiltWinSize, gyro_UAV_orig.measured), accel_cam_orig.tFloat)); % gyro_UAV_dsamp.measured(isnan(gyro_UAV_dsamp.measured))=0;
+			
+			% Apply the derivative filter to convert pos_cam_orig.measured to vel and accel
+			vel_cam_orig.measured = derivFilter(reshape(pos_cam_orig.measured, 1,[]), 1, mean(diff(pos_cam_orig.tFloat)), derivPolyOrder, derivWinSize);
+			accel_cam_orig.measured = derivFilter(reshape(pos_cam_orig.measured, 1,[]), 2, mean(diff(pos_cam_orig.tFloat)), derivPolyOrder, derivWinSize);
 
+			% Crop the variables according to accel_cam_orig.tFloat
             xl = [accel_cam_orig.tFloat(6), accel_UAV_orig.tFloat(end)];
             tCropInds = (accel_cam_orig.tFloat>=xl(1) & accel_cam_orig.tFloat<=xl(2));
             tCropIndsUAV = (accel_UAV_orig.tFloat>=xl(1) & accel_UAV_orig.tFloat<=xl(2));
@@ -29,6 +36,7 @@ function data = loadRealExperimentData(logInfo, logFolder)
 			accel_UAV_dsamp_crop = struct('tFloat',accel_UAV_dsamp.tFloat(tCropInds), 't',accel_cam_orig.tFloat(tCropInds)-accel_cam_orig.tFloat(find(tCropInds,1)), 'measured',accel_UAV_dsamp.measured(tCropInds));
             gyro_UAV_dsamp_crop = struct('tFloat',gyro_UAV_dsamp.tFloat(tCropInds), 't',accel_cam_orig.tFloat(tCropInds)-accel_cam_orig.tFloat(find(tCropInds,1)), 'measured',gyro_UAV_dsamp.measured(tCropInds));
 
+			% Save processed variables in output struct
             data(j).p_cam.(magnitudes{i}) = pos_cam_crop;
             data(j).v_cam.(magnitudes{i}) = vel_cam_crop;
             data(j).a_cam.(magnitudes{i}) = accel_cam_crop;
--- Experiment postprocessing/plotExperimentFigures.m
+++ Experiment postprocessing/plotExperimentFigures.m
@@ -1,2 +1,7 @@
-data = loadRealExperimentData(struct('datetime',{'2017-10-01 02-38-08'}, 'ch','80'));
-figure; plot(data.a_UAV_orig.Z.t, data.a_UAV_orig.Z.measured, 'g', 'LineWidth',2); hold on; plot(data.a_UAV.Z.t, data.a_UAV.Z.measured, 'r', 'LineWidth',2); plot(data.a_cam.Z.t, data.a_cam.Z.measured, 'b', 'LineWidth',2); legend('a_{UAV},raw', 'a_{UAV},filt', 'a_{cam}');
+data = loadRealExperimentData(struct('datetime',{'2017-10-02 11-45-57'}, 'ch','80'), [], 2, 13, 30);
+UAVgravityFactor = 9.81;
+for strAxCell = {'X', 'Y', 'Z'}
+	strAx = strAxCell{:};
+	figure; plot(data.a_UAV_orig.(strAx).t, UAVgravityFactor.*data.a_UAV_orig.(strAx).measured, 'g', 'LineWidth',2); hold on; plot(data.a_UAV.(strAx).t, UAVgravityFactor.*data.a_UAV.(strAx).measured, 'r', 'LineWidth',2); plot(data.a_cam.(strAx).t, data.a_cam.(strAx).measured, 'b', 'LineWidth',2); legend('a_{UAV},raw', 'a_{UAV},filt', 'a_{cam}');
+	figure; plot(data.p_cam.(strAx).t, data.p_cam.(strAx).measured, 'b', 'LineWidth',2);
+end
